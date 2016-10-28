329f9ed67a1eedb1c5291cbd447f78cf9e2b7c5e
salvador.sanjuan@gmail.com <salvador.sanjuan@gmail.com>
2016-10-28 21:35:44 +0200

    Feat: Insert and Read methods implemented

--- app/src/main/java/com/example/android/habittracker/MainActivity.java
+++ app/src/main/java/com/example/android/habittracker/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.habittracker;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -25,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public boolean Insert() {
+    public long Insert() {
 
         // Gets the database in write mode
         SQLiteDatabase db = mDbHelper.getWritableDatabase();
@@ -40,13 +41,31 @@ public class MainActivity extends AppCompatActivity {
         values.put(HabitTracker.COLUMN_DURATION, "03:30" );
         values.put(HabitTracker.COLUMN_COMMENTS, "5 kilometers");
 
-        
+        // Insert the new row, returning the primary key value of the new row
+        long new_row_id = db.insert(HabitTracker.TABLE_NAME,null,values);
 
 
+        return new_row_id;
 
+    }
 
+    public Cursor Read() {
 
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
+        // Make a Query using the query method from the SQLiteDatabase Class
+
+        Cursor cursor = db.query(HabitTracker.TABLE_NAME,
+                null,       // The table to query
+                null,       // The columns to return
+                null,       // The columns for the WHERE clause
+                null,       // The values for the WHERE clause
+                null,       // don't group the rows
+                null        // don't filter by row groups
+        );
+
+        return cursor;
     }
 
 }
